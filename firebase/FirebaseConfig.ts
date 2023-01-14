e7cf9ec40c2a6331caaf226983ba7150ee72fb78
Adam <adamomarali37@gmail.com>
2023-01-14 14:00:34 -0500

    firebase app initialization

---
+++ firebase/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCE-iGF82CJNkCfeTYcfHJ63ph4G1Pn08Y",
+  authDomain: "scouting-app-2023.firebaseapp.com",
+  projectId: "scouting-app-2023",
+  storageBucket: "scouting-app-2023.appspot.com",
+  messagingSenderId: "831589681771",
+  appId: "1:831589681771:web:346e5a2eb3608ec44abf2e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

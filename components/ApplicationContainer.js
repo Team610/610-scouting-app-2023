82a4ca72621945c85ec614006eafe123ffe7c004
David Shan <davidshan20070302@gmail.com>
2023-01-28 19:04:00 -0500

    add project

---
+++ components/ApplicationContainer.js
@@ -0,0 +1,27 @@
+import {
+    AppShell,
+    Header,
+    Text
+} from "@mantine/core"
+
+export const ApplicationContainer = ({children}) => {
+    return(
+        <AppShell
+        styles={{
+            main:{background: "#191A19",
+        width: "100vw",
+    height: "100vh",
+paddingLeft:"0px",}
+        }}
+        header = {
+            <Header height = {70} p="md">
+                <div style={{display:"flex", alignItems: 'center', height:"100%"}}> 
+                    <Text size="xl" weight = "bolder">Team 610 Stopwatch</Text>
+                </div>
+            </Header>
+        }
+        >
+            {children}
+        </AppShell>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import {
+  Footer,
+  Text
+} from "@mantine/core"
+import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import myPic from '../assets/FRCGameField.jpg'
+import React from "react";
+
+export default function Home() {
+  const [coords, setCoords] = React.useState([]);
+  const [mousePos, setMousePos] = React.useState({});
+
+  function handleClick(){
+    setCoords(coords => [...coords, mousePos]);
+  }
+
+  React.useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener(
+        'mousemove',
+        handleMouseMove
+      );
+    };
+  }, []);
+
+  return (
+    <div>
+      <div style={{position: "relative"}}>
+        <Image  onClick={()=> handleClick()}
+          src = {myPic}
+          width = "1000"
+          left = "0"
+          up = "85"
+          position = "absolute"
+        />
+        {coords.map((coord) => {
+            return (
+              <div style={{left: coord.x, top:coord.y-85, position: "absolute"}}>
+                🧊
+              </div>
+            )
+        })}
+      </div>
+      
+      <Footer height={130} p="md">
+          {coords.map((coord) => {
+            return <span>({coord.x}, {coord.y})    </span>
+          })}
+      </Footer>
+    </div>
+  );
+}

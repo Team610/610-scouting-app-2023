42dfbb0f8f356ea2e41791b89ed3c1db5776371f
David Shan <davidshan20070302@gmail.com>
2023-01-28 22:47:16 -0500

    added option to choose cone, cube, or none

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import {
+  Button,
   Footer,
+  Group,
   Text
 } from "@mantine/core"
 import styles from '../styles/Home.module.css'
@@ -10,9 +12,25 @@ import React from "react";
 export default function Home() {
   const [coords, setCoords] = React.useState([]);
   const [mousePos, setMousePos] = React.useState({});
+  const [gamePiece, setGamePiece] = React.useState(0);
 
   function handleClick(){
-    setCoords(coords => [...coords, mousePos]);
+    if(gamePiece!=0){
+      setCoords(coords => [...coords, [mousePos.x,mousePos.y,gamePiece]]);
+    }
+    console.log(coords);
+  }
+
+  function handleConeClick(){
+    setGamePiece(1); 
+  }
+
+  function handleNoneClick(){
+    setGamePiece(0);
+  }
+
+  function handleCubeClick(){
+    setGamePiece(2);
   }
 
   React.useEffect(() => {
@@ -32,26 +50,31 @@ export default function Home() {
 
   return (
     <div>
+      <Group position="left" spacing="xl" >
+        <Button onClick={()=> handleCubeClick()}> Cube</Button>
+        <Button onClick={()=> handleConeClick()}> Cone</Button>
+        <Button onClick={()=> handleNoneClick()}> None</Button>
+      </Group>
       <div style={{position: "relative"}}>
         <Image  onClick={()=> handleClick()}
           src = {myPic}
-          width = "1000"
+          width = "900"
           left = "0"
           up = "85"
           position = "absolute"
         />
         {coords.map((coord) => {
             return (
-              <div style={{left: coord.x, top:coord.y-85, position: "absolute"}}>
-                🧊
-              </div>
+                <text style={{left: coord[0], top:coord[1]-85-37, position: "absolute"}}>
+                  {coord[2]===2 ? '🧊' : ''}
+                  {coord[2]===1 ? '⚠':''}
+                </text>
             )
         })}
       </div>
-      
       <Footer height={130} p="md">
           {coords.map((coord) => {
-            return <span>({coord.x}, {coord.y-85})    </span>
+            return <span>({coord[0]}, {coord[1]-85-37})    </span>
           })}
       </Footer>
     </div>

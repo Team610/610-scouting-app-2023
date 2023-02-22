f914dd83f499b3daa3a67fa74919b2e07e4696c8
Adam <adamomarali37@gmail.com>
2023-02-22 17:21:16 -0500

    aggrregate work start

---
+++ neo4j/Teams.ts
@@ -0,0 +1,14 @@
+import { getNeoSession } from "./Session";
+
+export async function getTeams(){
+    const session = getNeoSession()
+    const tx = session.beginTransaction()
+    try{
+
+        const res = await tx.run('MATCH (n:Team) return (n)')
+        return res.records[2].get(0).properties.name
+
+    }catch(e){
+        console.log(e)
+    }
+}
---
+++ pages/[...team]/index.tsx
@@ -0,0 +1,3 @@
+export default function TeamView() {
+  return <div>Hi!</div>;
+}

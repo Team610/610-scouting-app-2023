b8d52bc431ae9d739af16361bf109de96f8b0d5f
Mathis Luo <saltcoffee.mathis@gmail.com>
2023-02-27 16:18:53 -0500

    team urls working

--- pages/teams.tsx
+++ pages/teams.tsx
@@ -1,12 +1,17 @@
 import { Anybody } from "@next/font/google";
 import { getTeams } from "../neo4j/teams";
-
+import { useRouter } from 'next/router'
 
 export default function Home({ data }: any) {
-    // console.log(data);
+    const router = useRouter();
     let element = [];
+    element.push(<div style={{ border: "1px solid grey" }}>Teams</div>);
     for (let index = 0; index < data.length; index++) {
-        element.push((<div  style={{border: "1px solid grey"}}>{data[index]}</div>))
+        element.push((
+            <div style={{ border: "1px solid grey" }}>
+                <button onClick={() => router.push('/teams/'+data[index])}>{data[index]}</button>
+            </div>
+        ))
     }
     return (
         <div>

dc5b4f3e05f985dde86b69023b0a613207743827
Adam <adamomarali37@gmail.com>
2023-02-28 14:52:58 -0500

    plz

--- pages/TeamPage.tsx
+++ pages/TeamPage.tsx
@@ -1,23 +1,23 @@
 import { Anybody } from "@next/font/google";
 import { useRouter } from "next/router";
-import { getTeams } from "../neo4j/teams";
+// import { getTeams } from "../neo4j/teams";
 
 function Home({ data }: any) {
-  const router = useRouter();
-  let element = [];
-  element.push(<div style={{ border: "1px solid grey" }}>Teams</div>);
-  for (let index = 0; index < data.length; index++) {
-    element.push(
-      <div style={{ border: "1px solid grey" }}>
-        <button onClick={() => router.push("/teams/" + data[index])}>
-          {data[index]}
-        </button>
-      </div>
-    );
-  }
-  return <div>{element}</div>;
+  // const router = useRouter();
+  // let element = [];
+  // element.push(<div style={{ border: "1px solid grey" }}>Teams</div>);
+  // for (let index = 0; index < data.length; index++) {
+  //   element.push(
+  //     <div style={{ border: "1px solid grey" }}>
+  //       <button onClick={() => router.push("/teams/" + data[index])}>
+  //         {data[index]}
+  //       </button>
+  //     </div>
+  //   );
+  // }
+  return <div></div>;
 }
 
 export async function getServerSideProps() {
-  return { props: { data: await getTeams() } };
+  // return { props: { data: await getTeams() } };
 }

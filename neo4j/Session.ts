184a79b1d98d0bca656abb48efbf1d16766fa872
Adam <adamomarali37@gmail.com>
2023-02-28 13:44:26 -0500

    switch to env

--- neo4j/Session.ts
+++ neo4j/Session.ts
@@ -1,7 +1,7 @@
 import neo4j from 'neo4j-driver'
 
-const uri:string = "bolt://localhost:7687"
-const driver = neo4j.driver(uri, neo4j.auth.basic("neo4j", "robotics"))
+const uri:string = process.env.NEO4J_URI!
+const driver = neo4j.driver(uri, neo4j.auth.basic(process.env.DATABASE_NAME!, process.env.DATABASE_PASSWORD!))
 
 export function getNeoSession(){
     let session = driver.session()

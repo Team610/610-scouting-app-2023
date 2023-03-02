efab4808be5366c22483da1f6710419f80bfe54d
parth-r-patel <parth.p.188@gmail.com>
2023-03-02 02:41:40 -0500

    Update matches.tsx

--- pages/matches.tsx
+++ pages/matches.tsx
@@ -11,15 +11,12 @@ export default function SelectMatchDropBox() {
         async function fetchData() {
             const headers = {
                 "X-TBA-Auth-Key": "monpiIlPoQ81Y5bc8zhoMNuTbm8bLHQzYikSQuYYZHvM3BbAm8Y4uFeaOU6bMNg1",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
             }
-            const response = await fetch("https://www.thebluealliance.com/api/v3/event/2023week0/matches", { headers: headers });
+            const response = await fetch("https://www.thebluealliance.com/api/v3/event/2023isde1/matches", { headers: headers });
             const data = await response.json();
             setJsonData(data);
-            setMatchNumbers(data.map((item: { match_number: any; }) => item.match_number));
-    
+            setMatchNumbers(data.map((item: { match_number: number, key: any; }) => item.key));
+
 
         }
         fetchData();

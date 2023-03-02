5a8d639bd7c6a1b46c45f8ef8730bee770b0fc5a
Adam <adamomarali37@gmail.com>
2023-03-01 21:00:52 -0500

    plz

--- neo4j/AddData.ts
+++ neo4j/AddData.ts
@@ -18,8 +18,8 @@ export interface matchData {
     autoClimb: number,
     teleopClimb: number,
     numPartners: number,
-    park: number,
-    mobility: number,
+    park: boolean,
+    mobility: boolean,
     cycles: Array<cycleData>,
     enemies: Array<number>,
     allies: Array<number>

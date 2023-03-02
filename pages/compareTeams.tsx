ac9a73aea45513664fef4dbf9c4efdc33cd10c1f
Adam <adamomarali37@gmail.com>
2023-03-01 20:55:52 -0500

    plz

--- pages/compareTeams.tsx
+++ pages/compareTeams.tsx
@@ -6,81 +6,89 @@ import sampleMatch from "../data/sampleMatch.json";
 import { Input } from "@mantine/core";
 import { useEffect, useState } from "react";
 
-export function CompareTeamData({ teams }: { teams: Array<number>}) {
-    const [data6, setData6] = useState<teamData[]>()
-    const teamRoles = ["Blue 1", "Blue 2", "Blue 3", "Red 1", "Red 2", "Red 3"]
+export function CompareTeamData({ teams }: { teams: Array<number> }) {
+  const [data6, setData6] = useState<teamData[]>();
+  const teamRoles = ["Blue 1", "Blue 2", "Blue 3", "Red 1", "Red 2", "Red 3"];
 
-    useEffect(() => {
-        async function getData() {
-            console.log("getting data")
-            setData6(await getCompTeams(teams))
-        }
-        getData()
-    }, [])
+  useEffect(() => {
+    async function getData() {
+      console.log("getting data");
+      setData6(await getCompTeams(teams));
+    }
+    getData();
+  }, []);
 
-    const ths = (
-        <tr>
-            <th>Team</th>
-            <th>In this game</th>
-            <th>Matches Played</th>
-            <th>Auto PPG</th>
-            <th>PPG</th>
-            <th>Cycles per Game</th>
-            <th>Scoring Accuracy</th>
-            <th>Cone Accuracy</th>
-            <th>Cube Accuracy</th>
-            <th>Scoring Positions</th>
-            <th>Auto Climb PPG</th>
-            <th>Teleop Climb PPG</th>
-            <th>Climb PPG</th>
-            <th>Link PG</th>
+  const ths = (
+    <tr>
+      <th>Team</th>
+      <th>In this game</th>
+      <th>Matches Played</th>
+      <th>Auto PPG</th>
+      <th>PPG</th>
+      <th>Cycles per Game</th>
+      <th>Scoring Accuracy</th>
+      <th>Cone Accuracy</th>
+      <th>Cube Accuracy</th>
+      <th>Scoring Positions</th>
+      <th>Auto Climb PPG</th>
+      <th>Teleop Climb PPG</th>
+      <th>Climb PPG</th>
+      <th>Link PG</th>
+    </tr>
+  );
 
-        </tr>
-    );
+  const rows = data6 ? (
+    data6.map((data: teamData, index: number) => (
+      <tr key={data.team}>
+        <td>{data.team}</td>
+        <td>{teamRoles[index]}</td>
+        <td>{data.matchesPlayed}</td>
+        <td>{data.autoPPG}</td>
+        <td>{data.PPG}</td>
+        <td>{data.cyclesPG}</td>
+        <td>{data.scoringAccuracy}</td>
+        <td>{data.coneAccuracy}</td>
+        <td>{data.cubeAccuracy}</td>
+        <td>
+          {"Lower: " +
+            data.scoringPositions[0] +
+            " Middle: " +
+            data.scoringPositions[1] +
+            " Top: " +
+            data.scoringPositions[2]}
+        </td>
+        <td>{data.autoClimbPPG}</td>
+        <td>{data.teleopClimbPPG}</td>
+        <td>{data.climbPPG}</td>
+        <td>{data.linkPG}</td>
+      </tr>
+    ))
+  ) : (
+    <></>
+  );
 
-
-    const rows = data6 ? data6.map((data : teamData, index : number) => (
-        <tr key={data.team}>
-             <td>{data.team}</td>
-             <td>{teamRoles[index]}</td>
-             <td>{data.matchesPlayed}</td>
-             <td>{data.autoPPG}</td>
-             <td>{data.PPG}</td>
-             <td>{data.cyclesPG}</td>
-             <td>{data.scoringAccuracy}</td>
-             <td>{data.coneAccuracy}</td>
-             <td>{data.cubeAccuracy}</td>
-             <td>{"Lower: " + data.scoringPositions[0] + " Middle: " + data.scoringPositions[1] + " Top: " + data.scoringPositions[2]}</td>
-             <td>{data.autoClimbPPG}</td>
-             <td>{data.teleopClimbPPG}</td>
-             <td>{data.climbPPG}</td>
-             <td>{data.linkPG}</td>
-        </tr>
-    )) : <></>;
-
-
-    return (
-        <>
-            <Table>
-                <thead>{ths}</thead>
-                <tbody>{rows}</tbody>
-            </Table>
-        </>
-    );
+  return (
+    <>
+      <Table>
+        <thead>{ths}</thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </>
+  );
 }
 
 export default function CompareTeams() {
-    return (
-        <div>
-            <Button onClick={async () => await createNTeams(20)}>Create dummy teams</Button>
-            <Button onClick={async () => await addDummyData({ data: sampleMatch })}>
+  return (
+    <div>
+      <Button onClick={async () => await createNTeams(20)}>
+        Create dummy teams
+      </Button>
+      {/* <Button onClick={async () => await addDummyData({ data: sampleMatch })}>
                 Add dummy data
-            </Button>
-            <Button onClick={async () => await wipe()}>
-                Wipe
-            </Button>
+            </Button> */}
+      <Button onClick={async () => await wipe()}>Wipe</Button>
 
-            <CompareTeamData teams={[1, 2, 3, 4, 5, 6]}/>
-        </div>
-    );
+      <CompareTeamData teams={[1, 2, 3, 4, 5, 6]} />
+    </div>
+  );
 }

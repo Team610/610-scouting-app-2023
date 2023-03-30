8043451b9c16e75bb986810c725bd23db65db835
David Shan <davidshan20070302@gmail.com>
2023-03-30 19:13:59 -0400

    added defense timer

--- neo4j/Session.ts
+++
@@ -1,9 +0,0 @@
-import neo4j from 'neo4j-driver'
-
-const uri:string = process.env.NEXT_PUBLIC_DATABASE_URI!
-const driver = neo4j.driver(uri, neo4j.auth.basic(process.env.NEXT_PUBLIC_DATABASE_NAME!, process.env.NEXT_PUBLIC_DATABASE_PASSWORD!))
-
-export function getNeoSession(){
-    let session = driver.session({ database: "neo4j" })
-    return session
-}

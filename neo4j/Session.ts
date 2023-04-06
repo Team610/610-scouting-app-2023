fb480fcef8c8e9640d364ea9a7655c79cc20cd2e
Adam <adamomarali37@gmail.com>
2023-04-05 21:48:31 -0400

    change

--- neo4j/Session.ts
+++ neo4j/Session.ts
@@ -1,9 +1,9 @@
 import neo4j from 'neo4j-driver'
 
 const uri:string = process.env.NEXT_PUBLIC_DATABASE_URI!
-const driver = neo4j.driver(uri, neo4j.auth.basic(process.env.NEXT_PUBLIC_DATABASE_USERNAME!, process.env.NEXT_PUBLIC_DATABASE_PASSWORD!))
+const driver = neo4j.driver(uri, neo4j.auth.basic("neo4j", process.env.NEXT_PUBLIC_DATABASE_PASSWORD!))
 
 export function getNeoSession(){
-    let session = driver.session({ database: process.env.NEXT_PUBLIC_DATABASE_NAME })
+    let session = driver.session({ database: "northbay" })
     return session
 }
